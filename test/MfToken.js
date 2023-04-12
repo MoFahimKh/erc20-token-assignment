5661ff5d8fd5d5509e3225e3472fdc483ae44fe9
M Fahim KHAN <mkhan@qodeleaf.com>
2023-04-12 18:03:43 +0530

    Update scripts/test/MfoTken.js

--- test/MfToken.js
+++ test/MfToken.js
@@ -1,13 +1,13 @@
 const { expect } = require("chai");
 
 describe("MfToken", function () {
-  let token, hardhatToken, owner, address1, address2, address3;
+  let token, hardhatToken, owner, user1, user2, user3;
 
   const initialSupply = ethers.utils.parseUnits("1000", 8);
 
   beforeEach(async () => {
     token = await ethers.getContractFactory("MfToken");
-    [owner, address1, address2, address3] = await ethers.getSigners();
+    [owner, user1, user2, user3] = await ethers.getSigners();
     hardhatToken = await token.deploy(initialSupply);
   });
 
@@ -32,60 +32,58 @@ describe("MfToken", function () {
   describe("Transfer", function () {
     it("should transfer tokens from sender to receiver", async () => {
       const amount = ethers.utils.parseUnits("100", 8);
-      await hardhatToken.transfer(address1.address, amount);
+      await hardhatToken.transfer(user1.address, amount);
       expect(await hardhatToken.balanceOf(owner.address)).to.equal(
         initialSupply.sub(amount)
       );
-      expect(await hardhatToken.balanceOf(address1.address)).to.equal(amount);
+      expect(await hardhatToken.balanceOf(user1.address)).to.equal(amount);
     });
     it("should emit Transfer event on successful transfer", async () => {
       const amount = ethers.utils.parseUnits("100", 8);
-      const tx = await hardhatToken.transfer(address1.address, amount);
+      const tx = await hardhatToken.transfer(user1.address, amount);
       expect(tx)
         .to.emit(hardhatToken, "Transfer")
-        .withArgs(owner.address, address1.address, amount);
+        .withArgs(owner.address, user1.address, amount);
     });
     it("should fail when sender doesn't have enough balance", async () => {
       const amount = ethers.utils.parseUnits("10000", 8);
-      await expect(hardhatToken.transfer(address1.address, amount)).to.be
-        .reverted;
+      await expect(hardhatToken.transfer(user1.address, amount)).to.be.reverted;
     });
     it("should fail when transferring zero tokens", async () => {
       const amount = ethers.utils.parseUnits("0", 8);
-      await expect(hardhatToken.transfer(address1.address, amount)).to.be
-        .reverted;
+      await expect(hardhatToken.transfer(user1.address, amount)).to.be.reverted;
     });
   });
 
   describe("Approve", function () {
     it("should approve spender to spend sender's tokens", async () => {
       const amount = ethers.utils.parseUnits("100", 8);
-      await hardhatToken.approve(address1.address, amount);
+      await hardhatToken.approve(user1.address, amount);
       expect(
-        await hardhatToken.allowance(owner.address, address1.address)
+        await hardhatToken.allowance(owner.address, user1.address)
       ).to.equal(amount);
     });
 
     it("should emit Approval event on successful approval", async () => {
       const amount = ethers.utils.parseUnits("100", 8);
-      const tx = await hardhatToken.approve(address1.address, amount);
+      const tx = await hardhatToken.approve(user1.address, amount);
       expect(tx)
         .to.emit(hardhatToken, "Approval")
-        .withArgs(owner.address, address1.address, amount);
+        .withArgs(owner.address, user1.address, amount);
     });
   });
 
   describe("TransferFrom", function () {
     it("should transfer tokens from approved sender to receiver", async () => {
       const amount = ethers.utils.parseUnits("100", 8);
-      await hardhatToken.approve(address1.address, amount);
-      await hardhatToken.transferFrom(owner.address, address2.address, amount, {
-        from: address1.address,
+      await hardhatToken.approve(user1.address, amount);
+      await hardhatToken.transferFrom(owner.address, user2.address, amount, {
+        from: user1.address,
       });
       expect(await hardhatToken.balanceOf(owner.address)).to.equal(
         initialSupply.sub(amount)
       );
-      expect(await hardhatToken.balanceOf(address2.address)).to.equal(amount);
+      expect(await hardhatToken.balanceOf(user2.address)).to.equal(amount);
     });
   });
 });
