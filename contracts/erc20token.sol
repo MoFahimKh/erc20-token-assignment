ed0b59b0a9e337b00825f2a5c25c1c3418090bc7
M Fahim KHAN <mkhan@qodeleaf.com>
2023-04-12 19:23:16 +0530

    Update MfToken.sol contract

--- contracts/erc20token.sol
+++ contracts/erc20token.sol
@@ -2,55 +2,77 @@
 pragma solidity ^0.8.9;
 
 interface ERC20Interface {
-    function balanceOf(address tokenOwner) external view returns (uint balance);
+    function balanceOf(
+        address tokenOwner
+    ) external view returns (uint256 balance);
 
     function allowance(
         address tokenOwner,
         address spender
-    ) external view returns (uint remaining);
+    ) external view returns (uint256 remaining);
 
-    function transfer(address to, uint tokens) external returns (bool success);
+    function transfer(
+        address to,
+        uint256 tokens
+    ) external returns (bool success);
 
     function approve(
         address spender,
-        uint tokens
+        uint256 tokens
     ) external returns (bool success);
 
     function transferFrom(
         address from,
         address to,
-        uint tokens
+        uint256 tokens
     ) external returns (bool success);
 
-    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
     event Approval(
         address indexed tokenOwner,
         address indexed spender,
-        uint tokens
+        uint256 tokens
     );
 }
 
 contract MfToken is ERC20Interface {
-    string public constant name = "Mf Token";
-    string public constant symbol = "MFT";
-    uint8 public constant decimals = 8;
-    uint256 public totalSupply;
-    mapping(address => uint256) balances;
-    mapping(address => mapping(address => uint256)) allowed;
+    string private name;
+    address private owner;
+    string private symbol;
+    uint8 private decimals;
+    uint256 private totalSupply;
+    mapping(address => uint256) internal balances;
+    mapping(address => mapping(address => uint256)) internal allowed;
 
     constructor(uint256 initialSupply) {
+        name = "MfToken";
+        symbol = "MFT";
         totalSupply = initialSupply;
+        decimals = 8;
         balances[msg.sender] = initialSupply;
+        owner = msg.sender;
     }
 
-    function balanceOf(address _owner) public view returns (uint256) {
+    function getName() external view returns (string memory) {
+        return name;
+    }
+
+    function getSymbol() external view returns (string memory) {
+        return symbol;
+    }
+
+    function getDecimals() external view returns (uint8) {
+        return decimals;
+    }
+
+    function balanceOf(address _owner) external view returns (uint256) {
         return balances[_owner];
     }
 
     function transfer(
         address _to,
         uint256 _value
-    ) public returns (bool success) {
+    ) external returns (bool success) {
         require(balances[msg.sender] >= _value && _value > 0);
         balances[msg.sender] -= _value;
         balances[_to] += _value;
@@ -61,7 +83,7 @@ contract MfToken is ERC20Interface {
     function approve(
         address _spender,
         uint256 _value
-    ) public returns (bool success) {
+    ) external returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
@@ -70,7 +92,7 @@ contract MfToken is ERC20Interface {
     function allowance(
         address _owner,
         address _spender
-    ) public view returns (uint256) {
+    ) external view returns (uint256) {
         return allowed[_owner][_spender];
     }
 
@@ -78,7 +100,7 @@ contract MfToken is ERC20Interface {
         address _from,
         address _to,
         uint256 _value
-    ) public returns (bool success) {
+    ) external returns (bool success) {
         require(
             balances[_from] >= _value &&
                 allowed[_from][msg.sender] >= _value &&
@@ -89,5 +111,20 @@ contract MfToken is ERC20Interface {
         balances[_to] += _value;
         emit Transfer(_from, _to, _value);
         return true;
+    }
+
+    function mint(address _to, uint256 _amount) external {
+        require(msg.sender == owner);
+        require(totalSupply + _amount >= totalSupply);
+        totalSupply += _amount;
+        balances[_to] += _amount;
+        emit Transfer(address(0), _to, _amount);
+    }
+
+    function burn(uint256 _amount) external {
+        require(balances[msg.sender] >= _amount && _amount > 0);
+        totalSupply -= _amount;
+        balances[msg.sender] -= _amount;
+        emit Transfer(msg.sender, address(0), _amount);
     }
 }
