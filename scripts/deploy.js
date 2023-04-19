0d9402a33ce2855c442e1cc7ea8bae91d7f6e88b
MfahimKh <mkhan@qodeleaf.com>
2023-04-19 13:22:37 +0530

    Update deploy script: 	*Add ERC721 delpoy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,11 +7,18 @@
 const hre = require("hardhat");
 
 async function main() {
+  //erc20 deploy script
  const [deployer] = await ethers.getSigners();
-const initialSupply =  ethers.utils.parseUnits("1000", 8);
+ console.log(`Deployer address : ${deployer.address}`);
+const initialSupply =  1000000000000000; 
  const Token = await ethers.getContractFactory('MfToken');
  const token = await Token.deploy(initialSupply);
  console.log(`Token address : ${token.address}`);
+
+  //erc721 deploy script
+const NFT = await ethers.getContractFactory('MfNft');
+ const nft = await NFT.deploy("MfNft","MFT");
+ console.log(`NFT address : ${nft.address}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
