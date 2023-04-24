952cc8d6fc7619ebb12994cebbcb294840bc6c63
MfahimKh <mkhan@qodeleaf.com>
2023-04-24 16:35:53 +0530

    Update interface

--- contracts/IERC1155Receiver.sol
+++ contracts/IERC1155Receiver.sol
@@ -19,23 +19,4 @@ interface IERC1155Receiver {
     ) external returns (bytes4);
 }
 
-//  function mintTo(
-//         address _to,
-//         uint256 _tokenId,
-//         string memory _uri,
-//         uint256 _amount
-//     ) external {
-//         require(owner == msg.sender, "Only owner can mint");
-//         uint256 tokenIdToMint;
-//         if (_tokenId == type(uint256).max) {
-//             tokenIdToMint = nextTokenIdToMint;
-//             nextTokenIdToMint += 1;
-//             _tokenUris[tokenIdToMint] = _uri;
-//         } else {
-//             require(_tokenId < nextTokenIdToMint, "This cannot be the tokenId, id doesnt exists");
-//             tokenIdToMint = _tokenId;
-//         }
-//         _balances[tokenIdToMint][_to] += _amount;
-//         totalSupply[tokenIdToMint] += _amount;
-//         emit TransferSingle(msg.sender, address(0), _to, _tokenId, _amount);
-//     }
+
