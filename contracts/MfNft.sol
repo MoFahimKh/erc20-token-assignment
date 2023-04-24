c66eef81d5aeb119c3dc452a5942677eb84bd7c8
MfahimKh <mkhan@qodeleaf.com>
2023-04-25 00:07:31 +0530

    Update ERC20 and ERC721, use interface of open zeppelin

--- contracts/MfNft.sol
+++ contracts/MfNft.sol
@@ -10,41 +10,143 @@ interface IERC721Receiver {
     ) external returns (bytes4);
 }
 
-interface ERC721Interface {
-    function balanceOf(address owner) external view returns (uint256 balance);
+/// @title ERC-721 Non-Fungible Token Standard
+/// @dev See https://eips.ethereum.org/EIPS/eip-721
+///  Note: the ERC-165 identifier for this interface is 0x80ac58cd.
+/* is ERC165 */ interface ERC721 {
+    /// @dev This emits when ownership of any NFT changes by any mechanism.
+    ///  This event emits when NFTs are created (`from` == 0) and destroyed
+    ///  (`to` == 0). Exception: during contract creation, any number of NFTs
+    ///  may be created and assigned without emitting Transfer. At the time of
+    ///  any transfer, the approved address for that NFT (if any) is reset to none.
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 indexed _tokenId
+    );
 
-    function ownerOf(uint256 tokenId) external view returns (address owner);
+    /// @dev This emits when the approved address for an NFT is changed or
+    ///  reaffirmed. The zero address indicates there is no approved address.
+    ///  When a Transfer event emits, this also indicates that the approved
+    ///  address for that NFT (if any) is reset to none.
+    event Approval(
+        address indexed _owner,
+        address indexed _approved,
+        uint256 indexed _tokenId
+    );
 
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId,
-        bytes calldata data
-    ) external;
+    /// @dev This emits when an operator is enabled or disabled for an owner.
+    ///  The operator can manage all NFTs of the owner.
+    event ApprovalForAll(
+        address indexed _owner,
+        address indexed _operator,
+        bool _approved
+    );
 
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) external;
+    /// @notice Count all NFTs assigned to an owner
+    /// @dev NFTs assigned to the zero address are considered invalid, and this
+    ///  function throws for queries about the zero address.
+    /// @param _owner An address for whom to query the balance
+    /// @return The number of NFTs owned by `_owner`, possibly zero
+    function balanceOf(address _owner) external view returns (uint256);
 
-    function transferFrom(address from, address to, uint256 tokenId) external;
+    /// @notice Find the owner of an NFT
+    /// @dev NFTs assigned to zero address are considered invalid, and queries
+    ///  about them do throw.
+    /// @param _tokenId The identifier for an NFT
+    /// @return The address of the owner of the NFT
+    function ownerOf(uint256 _tokenId) external view returns (address);
 
-    function approve(address to, uint256 tokenId) external;
+    /// @notice Transfers the ownership of an NFT from one address to another address
+    /// @dev Throws unless `msg.sender` is the current owner, an authorized
+    ///  operator, or the approved address for this NFT. Throws if `_from` is
+    ///  not the current owner. Throws if `_to` is the zero address. Throws if
+    ///  `_tokenId` is not a valid NFT. When transfer is complete, this function
+    ///  checks if `_to` is a smart contract (code size > 0). If so, it calls
+    ///  `onERC721Received` on `_to` and throws if the return value is not
+    ///  `bytes4(keccak256("onERC721Received(address,address,uint256,bytes)"))`.
+    /// @param _from The current owner of the NFT
+    /// @param _to The new owner
+    /// @param _tokenId The NFT to transfer
+    /// @param data Additional data with no specified format, sent in call to `_to`
+    function safeTransferFrom_(
+        address _from,
+        address _to,
+        uint256 _tokenId,
+        bytes memory data
+    ) external payable;
 
-    function setApprovalForAll(address operator, bool approved) external;
+    /// @notice Transfers the ownership of an NFT from one address to another address
+    /// @dev This works identically to the other function with an extra data parameter,
+    ///  except this function just sets data to "".
+    /// @param _from The current owner of the NFT
+    /// @param _to The new owner
+    /// @param _tokenId The NFT to transfer
+    function safeTransferFro(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable;
 
-    function getApproved(
-        uint256 tokenId
-    ) external view returns (address operator);
+    /// @notice Transfer ownership of an NFT -- THE CALLER IS RESPONSIBLE
+    ///  TO CONFIRM THAT `_to` IS CAPABLE OF RECEIVING NFTS OR ELSE
+    ///  THEY MAY BE PERMANENTLY LOST
+    /// @dev Throws unless `msg.sender` is the current owner, an authorized
+    ///  operator, or the approved address for this NFT. Throws if `_from` is
+    ///  not the current owner. Throws if `_to` is the zero address. Throws if
+    ///  `_tokenId` is not a valid NFT.
+    /// @param _from The current owner of the NFT
+    /// @param _to The new owner
+    /// @param _tokenId The NFT to transfer
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _tokenId
+    ) external payable;
 
+    /// @notice Change or reaffirm the approved address for an NFT
+    /// @dev The zero address indicates there is no approved address.
+    ///  Throws unless `msg.sender` is the current NFT owner, or an authorized
+    ///  operator of the current owner.
+    /// @param _approved The new approved NFT controller
+    /// @param _tokenId The NFT to approve
+    function approve(address _approved, uint256 _tokenId) external payable;
+
+    /// @notice Enable or disable approval for a third party ("operator") to manage
+    ///  all of `msg.sender`'s assets
+    /// @dev Emits the ApprovalForAll event. The contract MUST allow
+    ///  multiple operators per owner.
+    /// @param _operator Address to add to the set of authorized operators
+    /// @param _approved True if the operator is approved, false to revoke approval
+    function setApprovalForAll(address _operator, bool _approved) external;
+
+    /// @notice Get the approved address for a single NFT
+    /// @dev Throws if `_tokenId` is not a valid NFT.
+    /// @param _tokenId The NFT to find the approved address for
+    /// @return The approved address for this NFT, or the zero address if there is none
+    function getApproved(uint256 _tokenId) external view returns (address);
+
+    /// @notice Query if an address is an authorized operator for another address
+    /// @param _owner The address that owns the NFTs
+    /// @param _operator The address that acts on behalf of the owner
+    /// @return True if `_operator` is an approved operator for `_owner`, false otherwise
     function isApprovedForAll(
-        address owner,
-        address operator
+        address _owner,
+        address _operator
     ) external view returns (bool);
 }
 
-contract MfNft is ERC721Interface {
+interface ERC165 {
+    /// @notice Query if a contract implements an interface
+    /// @param interfaceID The interface identifier, as specified in ERC-165
+    /// @dev Interface identification is specified in ERC-165. This function
+    ///  uses less than 30,000 gas.
+    /// @return `true` if the contract implements `interfaceID` and
+    ///  `interfaceID` is not 0xffffffff, `false` otherwise
+    function supportsInterface(bytes4 interfaceID) external view returns (bool);
+}
+
+contract MfNft is ERC721 {
     string private _name;
     string private _symbol;
     uint256 private _totalSupply;
@@ -53,22 +155,6 @@ contract MfNft is ERC721Interface {
     mapping(address => uint256) private _balances;
     mapping(uint256 => address) private _tokenApprovals;
     mapping(address => mapping(address => bool)) private _operatorApprovals;
-
-    event Transfer(
-        address indexed from,
-        address indexed to,
-        uint256 indexed tokenId
-    );
-    event Approval(
-        address indexed owner,
-        address indexed operator,
-        uint256 indexed tokenId
-    );
-    event ApprovalForAll(
-        address indexed owner,
-        address indexed operator,
-        bool approved
-    );
 
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
@@ -87,12 +173,14 @@ contract MfNft is ERC721Interface {
         return _totalSupply;
     }
 
-    function balanceOf(address owner) external view override returns (uint256) {
+    function balanceOf(
+        address _owner
+    ) external view override returns (uint256) {
         require(
-            owner != address(0),
+            _owner != address(0),
             "MfNft: balance query for the zero address"
         );
-        return _balances[owner];
+        return _balances[_owner];
     }
 
     function ownerOf(uint256 tokenId) public view override returns (address) {
@@ -111,19 +199,12 @@ contract MfNft is ERC721Interface {
         _mint(to, tokenId);
     }
 
-    function _mint(address _to, uint256 _amount) internal {
-        //require(_to != address(0));
-        _totalSupply += 1;
-        _balances[_to] += 1;
-        emit Transfer(address(0), _to, _amount);
-    }
-
-    function safeTransferFrom(
+    function safeTransferFrom_(
         address from,
         address to,
         uint256 tokenId,
         bytes memory data
-    ) public {
+    ) public payable override {
         transferFrom(from, to, tokenId);
         require(
             _checkOnERC721Received(from, to, tokenId, data),
@@ -131,15 +212,19 @@ contract MfNft is ERC721Interface {
         );
     }
 
-    function safeTransferFrom(
+    function safeTransferFro(
         address from,
         address to,
         uint256 tokenId
-    ) external {
-        safeTransferFrom(from, to, tokenId, "");
+    ) external payable override {
+        safeTransferFrom_(from, to, tokenId, "");
     }
 
-    function transferFrom(address from, address to, uint256 tokenId) public {
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public payable {
         require(
             ownerOf(tokenId) == from,
             "MfNft: transfer of token that is not own"
@@ -157,7 +242,7 @@ contract MfNft is ERC721Interface {
         emit Transfer(from, to, tokenId);
     }
 
-    function approve(address to, uint256 tokenId) external override {
+    function approve(address to, uint256 tokenId) external payable override {
         address owner = ownerOf(tokenId);
         require(to != owner, "MfNft: approval to current owner");
         require(
@@ -208,6 +293,14 @@ contract MfNft is ERC721Interface {
             isApprovedForAll(owner, spender));
     }
 
+    function clearApproval(uint256 tokenId) public {
+        require(
+            _exists(tokenId),
+            "MfNft: clearApproval tokenId does not exist"
+        );
+        _clearApproval(msg.sender, tokenId);
+    }
+
     function _clearApproval(address owner, uint256 tokenId) internal {
         require(
             ownerOf(tokenId) == owner,
@@ -231,23 +324,28 @@ contract MfNft is ERC721Interface {
         return size > 0;
     }
 
+    function _mint(address _to, uint256 _amount) internal {
+        //require(_to != address(0));
+        _totalSupply += 1;
+        _balances[_to] += 1;
+        emit Transfer(address(0), _to, _amount);
+    }
+
     function _checkOnERC721Received(
         address from,
         address to,
         uint256 tokenId,
         bytes memory data
     ) internal returns (bool) {
+        bytes4 ERC721_RECEIVED = 0x150b7a02;
         if (isContract(to)) {
-            bytes4 retval = IERC721Receiver(to).onERC721Received(
+            bytes4 returnValue = IERC721Receiver(to).onERC721Received(
                 msg.sender,
                 from,
                 tokenId,
                 data
             );
-            return (retval ==
-                bytes4(
-                    keccak256("onERC721Received(address,adrress,uint256,bytes)")
-                ));
+            return (returnValue == ERC721_RECEIVED);
         } else {
             return true;
         }
--- contracts/MfToken.sol
+++ contracts/MfToken.sol
@@ -1,46 +1,62 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-interface ERC20Interface {
-    function balanceOf(
-        address tokenOwner
-    ) external view returns (uint256 balance);
+interface Token {
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return balance the balance
+    function balanceOf(address _owner) external view returns (uint256 balance);
 
+    /// @notice send `_value` token to `_to` from `msg.sender`
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return success Whether the transfer was successful or not
+    function transfer(
+        address _to,
+        uint256 _value
+    ) external returns (bool success);
+
+    /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+    /// @param _from The address of the sender
+    /// @param _to The address of the recipient
+    /// @param _value The amount of token to be transferred
+    /// @return success Whether the transfer was successful or not
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) external returns (bool success);
+
+    /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @param _value The amount of wei to be approved for transfer
+    /// @return success Whether the approval was successful or not
+    function approve(
+        address _spender,
+        uint256 _value
+    ) external returns (bool success);
+
+    /// @param _owner The address of the account owning tokens
+    /// @param _spender The address of the account able to transfer the tokens
+    /// @return remaining Amount of remaining tokens allowed to spent
     function allowance(
-        address tokenOwner,
-        address spender
+        address _owner,
+        address _spender
     ) external view returns (uint256 remaining);
 
-    function transfer(
-        address to,
-        uint256 tokens
-    ) external returns (bool success);
-
-    function approve(
-        address spender,
-        uint256 tokens
-    ) external returns (bool success);
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokens
-    ) external returns (bool success);
-
-    event Transfer(address indexed from, address indexed to, uint256 tokens);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
     event Approval(
-        address indexed tokenOwner,
-        address indexed spender,
-        uint256 tokens
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
     );
 }
 
-contract MfToken is ERC20Interface {
+contract MfToken is Token {
     string private name;
     address private owner;
     string private symbol;
     uint8 private decimals;
-    uint256 private totalSupply;
+    uint256 private _totalSupply;
     mapping(address => uint256) internal balances;
     mapping(address => mapping(address => uint256)) internal allowed;
 
@@ -64,6 +80,10 @@ contract MfToken is ERC20Interface {
         return decimals;
     }
 
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
     function balanceOf(address _owner) external view returns (uint256) {
         return balances[_owner];
     }
@@ -82,7 +102,7 @@ contract MfToken is ERC20Interface {
     ) external returns (bool success) {
         allowed[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
-        return true;
+        success = true;
     }
 
     function allowance(
@@ -100,18 +120,18 @@ contract MfToken is ERC20Interface {
         require(allowed[_from][msg.sender] >= _value && _value > 0);
         allowed[_from][msg.sender] -= _value;
         _transfer(_from, _to, _value);
-        return true;
+        success = true;
     }
 
     function mint(address _to, uint256 _amount) external {
         require(msg.sender == owner);
-        require(totalSupply + _amount >= totalSupply);
+        require(_totalSupply + _amount >= _totalSupply);
         _mint(_to, _amount);
     }
 
     function burn(uint256 _amount) external {
         require(balances[msg.sender] >= _amount && _amount > 0);
-        totalSupply -= _amount;
+        _totalSupply -= _amount;
         balances[msg.sender] -= _amount;
         emit Transfer(msg.sender, address(0), _amount);
     }
@@ -127,7 +147,7 @@ contract MfToken is ERC20Interface {
 
     function _mint(address _to, uint256 _amount) internal {
         require(_to != address(0));
-        totalSupply += _amount;
+        _totalSupply += _amount;
         balances[_to] += _amount;
         emit Transfer(address(0), _to, _amount);
     }
